6400e0ab27642ed30a36f00a95629cca009b79c1
amch-med23 <skyied.med2023@gmail.com>
2024-04-15 17:09:06 +0100

    adding the python file that has the solution to the problem.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" lockboxes module """
+
+def canUnlockAll(boxes):
+    """ a function that determines if all the boxes can be opened """
+    if not boxes:
+        return False
+    size = len(boxes)
+    checker = {}
+    index = 0
+
+    for box in boxes:
+        if len(box) == 0 or index == 0:
+            checker[index] = -1
+        for key in box:
+            if key < size and key != index:
+                checker[key] = key
+        if len(checker) == size:
+            return True
+        index += 1
+    return False

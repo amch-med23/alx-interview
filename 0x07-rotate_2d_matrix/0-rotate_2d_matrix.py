4c41a167ef29c938712ec6ba30ccab8791e9c97e
amch-med23 <skyied.med2023@gmail.com>
2024-05-29 10:59:31 +0000

    modifying the code to use alternate naming

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ a script to retate a 2 dimensions matrix 90 degree clockwise """
 
 
 def rotate_2d_matrix(matrix):
     """ given n x n 2d matrix, retate it 90 degree clockwise """
-    first = 0
-    last = len(matrix) - 1
-    while first < last:
-        for i in range(first, last):
-            aux = matrix[i][last]
-            matrix[i][last] = matrix[first][i]
-            aux2 = matrix[last][last + first - i]
-            matrix[last][last + first - i] = aux
-            aux = matrix[last + first - i][first]
-            matrix[last + first - i][first] = aux2
-            matrix[first][i] = aux
+    f = 0
+    l = len(matrix) - 1
+    while f < l:
+        for i in range(f, l):
+            tmp = matrix[i][l]
+            matrix[i][l] = matrix[f][i]
+            tmp2 = matrix[l][l + f - i]
+            matrix[l][l + f - i] = tmp
+            tmp = matrix[l + f - i][f]
+            matrix[l + f - i][f] = tmp2
+            matrix[f][i] = tmp
             # aux is used here to swap values
         # increamenting the first (moving forward)
-        first += 1
+        f += 1
         # decreasing the last (moving backwards)
-        last -= 1
+        l -= 1

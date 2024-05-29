7b308cea826c9a34dc01c73aa196280396c67120
amch-med23 <skyied.med2023@gmail.com>
2024-05-29 11:02:29 +0000

    changing the naming convention (solve the ambiguous variable name)

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,18 +5,18 @@
 def rotate_2d_matrix(matrix):
     """ given n x n 2d matrix, retate it 90 degree clockwise """
     f = 0
-    l = len(matrix) - 1
-    while f < l:
-        for i in range(f, l):
-            tmp = matrix[i][l]
-            matrix[i][l] = matrix[f][i]
-            tmp2 = matrix[l][l + f - i]
-            matrix[l][l + f - i] = tmp
-            tmp = matrix[l + f - i][f]
-            matrix[l + f - i][f] = tmp2
+    last = len(matrix) - 1
+    while f < last:
+        for i in range(f, last):
+            tmp = matrix[i][last]
+            matrix[i][last] = matrix[f][i]
+            tmp2 = matrix[last][last + f - i]
+            matrix[last][last + f - i] = tmp
+            tmp = matrix[last + f - i][f]
+            matrix[last + f - i][f] = tmp2
             matrix[f][i] = tmp
             # aux is used here to swap values
         # increamenting the first (moving forward)
         f += 1
         # decreasing the last (moving backwards)
-        l -= 1
+        last -= 1

3357535972a9c447d20e42ba17bcf0c97c0e939c
amch-med23 <skyied.med2023@gmail.com>
2024-06-13 11:56:30 +0000

    fixing styling issues and renaming the main (perimeter) variable

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """ this script will solve the island perimeter problem """
 
+
 def island_perimeter(grid):
-    """ Returns the perimeter of the island described in grid """
+    """ the perimeter of the island described in grid """
 
     if not grid:
         return 0
-    perim = 0
+    per = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    perim += 1
+                    per += 1
                 if col == 0 or grid[row][col - 1] == 0:
-                    perim += 1
+                    per += 1
                 if row == len(grid) - 1 or grid[row + 1][col] == 0:
-                    perim += 1
+                    per += 1
                 if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
-                    perim += 1
-    return perim
+                    per += 1
+    return per

04ad03743659da0cc2320b166e9dc3aa81d530f8
amch-med23 <skyied.med2023@gmail.com>
2024-06-13 07:50:46 -0400

    adding the code for the island perimeter problem.

--- 0x09-island_perimeter/0-island_perimiter.py
+++ 0x09-island_perimeter/0-island_perimiter.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-""" this script will solve the island pirimeter problem """
+""" this script will solve the island perimeter problem """
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+
+    if not grid:
+        return 0
+    perim = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perim += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perim += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perim += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perim += 1
+    return perim
